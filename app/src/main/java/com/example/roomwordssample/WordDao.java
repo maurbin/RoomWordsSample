97155e1507d6e10feae7a91506f3ca36aafd28d2
maurbin <Diosmequiere0>
2021-03-15 23:59:35 +0100

    terminado

--- app/src/main/java/com/example/roomwordssample/WordDao.java
+++ app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -3,6 +3,7 @@ package com.example.roomwordssample;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
     LiveData<List<Word>> getAllWords();
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
     @Query("DELETE FROM word_table")
     void deleteAll();
